11325fc9611043800d9108e774cd9bccdafc819b
monsterkings <mbengonosalomon22@gmail.com>
2022-05-18 02:34:08 +0200

    Commit
    First one had little worries.

--- Salomon/mon-app/src/app/app.component.spec.ts
+++ Salomon/mon-app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'My-app'`, () => {
+  it(`should have as title 'mon-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('My-app');
+    expect(app.title).toEqual('mon-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('My-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mon-app app is running!');
   });
 });
